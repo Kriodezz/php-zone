b47d39ac6882cee80905649ef7ae30e00fa7ca40
Tarakanov Vyacheslav <kriodezz@gmail.com>
2022-06-07 16:18:53 +0500

    add cron

---
+++ bin/cli.php
@@ -0,0 +1,45 @@
+<?php
+
+try {
+
+    unset($argv[0]);
+
+    // Регистрируем функцию автозагрузки
+    spl_autoload_register(function (string $className) {
+        require_once __DIR__ . '/../src/' . $className . '.php';
+    });
+
+    // Составляем полное имя класса, добавив нэймспейс
+    $className = '\\MyProject\\Cli\\' . array_shift($argv);
+
+    if (!class_exists($className)) {
+        throw new \MyProject\Exceptions\CliException(
+            'Class "' . $className . '" not found'
+        );
+    }
+
+    if (!in_array('MyProject\Cli\AbstractCommand', class_parents($className))) {
+        throw new \MyProject\Exceptions\CliException(
+            'Class "' . $className . '" not allowed'
+        );
+    }
+
+
+    // Подготавливаем список аргументов
+    $params = [];
+    foreach ($argv as $argument) {
+        preg_match('/^-(.+)=(.+)$/', $argument, $matches);
+        if (!empty($matches)) {
+            $paramName = $matches[1];
+            $paramValue = $matches[2];
+
+            $params[$paramName] = $paramValue;
+        }
+    }
+
+    // Создаём экземпляр класса, передав параметры и вызываем метод execute()
+    $class = new $className($params);
+    $class->execute();
+} catch (\MyProject\Exceptions\CliException $e) {
+    echo 'Error: ' . $e->getMessage();
+}
